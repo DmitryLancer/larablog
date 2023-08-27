8cefcecf1fd16baea92cbf5fcf303e14d2836f9f
Dmitry <dmitrylancer.it@yandex.ru>
2023-08-27 08:43:55 +0300

    добавление категорий в бд

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\Category\CreateController;
+use App\Http\Controllers\Admin\Category\StoreController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers;
 use App\Http\Controllers\Main\IndexController;
@@ -29,13 +30,29 @@ Route::name('main.')->group(function() {
     Route::get('/', IndexController::class);
 });
 
+
+Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'], function () {
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', [CategoryController::class])->name('admin.index');
+    });
+
+    Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function () {
+        Route::get('/', CategoryController::class)->name('admin.category.index');
+        Route::get('/create', CreateController::class)->name('admin.category.create');
+        Route::post('/', StoreController::class)->name('admin.category.store');
+    });
+});
+
 Route::prefix('admin')->group(function (){
     Route::name('main')->group(function() {
         Route::get('/', AdminController::class);
     });
-    Route::name('main')->group(function() {
-        Route::get('/', AdminController::class);
-    });
+});
+
+
+//    Route::name('main')->group(function() {
+//        Route::get('/', AdminController::class);
+//    });
 
  // РАБОТАЕТ
 //    Route::prefix('categories')->group(function () {
@@ -65,17 +82,8 @@ Route::prefix('admin')->group(function (){
 //        });
 //    });
 
-    Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'], function () {
-        Route::group(['namespace' => 'Main'], function () {
-            Route::get('/', [CategoryController::class])->name('admin.index');
-        });
 
-        Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function () {
-            Route::get('/', CategoryController::class)->name('admin.category.index');
-            Route::get('/create', CreateController::class)->name('admin.category.create');
-        });
-    });
-});
+
 
 
 
