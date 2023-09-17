ee5bdaef7f48aaab8879b257b04298c806ac8ac3
Dmitry <dmitrylancer.it@yandex.ru>
2023-09-17 17:06:52 +0300

    случайные и популярные посты

--- app/Http/Controllers/Admin/Main/IndexController.php
+++ app/Http/Controllers/Admin/Main/IndexController.php
@@ -14,7 +14,9 @@ class IndexController extends Controller
 {
     public function __invoke()
     {
-
-        return view('main.index');
+        $posts = Post::paginate(6);
+        $randomPosts = Post::get()->random(4);
+        $likedPosts = Post::withCount('likedUsers')->orderBy('liked_users_count', 'DESC')->get()->take(4);
+        return view('main.index', compact('posts', 'randomPosts', 'likedPosts'));
     }
 }
