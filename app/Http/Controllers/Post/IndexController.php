3e2bec3a54d2e4df97d4536d2e155ea06cfcc030
Dmitry <dmitrylancer.it@yandex.ru>
2023-09-28 22:14:00 +0300

    финальна версия с норм постами

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -14,8 +14,8 @@ class IndexController extends Controller
 {
     public function __invoke()
     {
-        $posts = Post::paginate(6);
-        $randomPosts = Post::get()->random(4);
+        $posts = Post::paginate(4);
+        $randomPosts = Post::get()->random(2);
         $likedPosts = Post::withCount('likedUsers')->orderBy('liked_users_count', 'DESC')->get()->take(4);
         return view('post.index', compact('posts', 'randomPosts', 'likedPosts'));
     }
